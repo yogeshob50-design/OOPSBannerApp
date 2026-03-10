3ee871fecda6bab9ed7a8a40741dd7c8735dbdc4
yogeshob50-design <yogeshob50@gmail.com>
2026-03-10 23:25:42 +0530

    Add files via upload

---
+++ UC2.java
@@ -0,0 +1,11 @@
+public class UC2 {
+    public static void main(String[] args){
+        System.out.println("   ***   "+" "+"   ***   "+" "+"******   "+" "  + "   ***** ");
+        System.out.println(" **   ** "+" "+" **   ** "+" "+"**    ** "+" "  + " **      ");
+        System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "  + "**       ");
+        System.out.println("**     **"+" "+"**     **"+" "+"**    ** "+" "  + "  *****  ");
+        System.out.println("**     **"+" "+"**     **"+" "+"******   "+" "  + "      ** ");
+        System.out.println(" **   ** "+" "+" **   ** "+" "+"**       "+" "  + " **   ** ");
+        System.out.println("   ***   "+" "+"   ***   "+" "+"**       "+" "  + "  *****  ");
+    }
+}
