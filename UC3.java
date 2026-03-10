33644083e4d4d2f8e51788d79e5723cadc987d8c
yogeshob50-design <yogeshob50@gmail.com>
2026-03-10 23:26:29 +0530

    Add files via upload

---
+++ UC3.java
@@ -0,0 +1,11 @@
+public class UC3 {
+    public static void main(String[] args){
+        System.out.println(String.join(" ","   ***   ","   ***   ","******   ","   ***** "));
+        System.out.println(String.join(" "," **   ** "," **   ** ","**    ** "," **      "));
+        System.out.println(String.join(" ","**     **","**     **","**     **","**       "));
+        System.out.println(String.join(" ","**     **","**     **","**    ** ","  *****  "));
+        System.out.println(String.join(" ","**     **","**     **","******   ","      ** "));
+        System.out.println(String.join(" "," **   ** "," **   ** ","**       "," **   ** "));
+        System.out.println(String.join(" ","   ***   ","   ***   ","**       ","  *****  "));
+    }
+}
